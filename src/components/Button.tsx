89a63524c6d834c38be8b2ea5b55405e62f04665
Hector <torrezhectorb@gmail.com>
2023-08-29 19:59:21 -0600

    change button and navbar component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,17 +3,19 @@ import clsx from "clsx";
 interface ButtonProps {
   isAdd?: boolean;
   isDelete?: boolean;
+  isClose?: boolean;
   text: string;
   handleClick: () => void;
 }
 
 export const Button = (props: ButtonProps) => {
-  const { isAdd, isDelete, text, handleClick } = props;
+  const { isAdd, isDelete, text, handleClick, isClose } = props;
   return (
     <button
       className={clsx(
         isAdd && "bg-blue-400 text-white px-5 py-1 rounded-sm",
-        isDelete && "bg-red-400 text-white px-5 py-1 rounded-sm"
+        isDelete && "bg-red-400 text-white px-5 py-1 rounded-sm",
+        isClose && "bg-transparent text-red-400 px-5 py-1 rounded-sm"
       )}
       onClick={handleClick}
     >
--- src/features/Navbar/Navbar.tsx
+++ src/features/Navbar/Navbar.tsx
@@ -1,9 +1,17 @@
-export const Navbar = () => {
+interface NavbarProps {
+  onHandle: (boolean: boolean) => void;
+  formIsActive: boolean;
+}
+
+export const Navbar = ({ onHandle, formIsActive }: NavbarProps) => {
   return (
     <header className="flex justify-between m-auto max-w-5xl px-5 py-3 rounded-b-xl bg-gray-200 w-full">
       <h1 className="font-bold text-lg">CatBook</h1>
       <section>
-        <button className="font-semibold text-base outline-none border-none">
+        <button
+          onClick={() => onHandle(!formIsActive)}
+          className="font-semibold text-base outline-none border-none"
+        >
           Upload
         </button>
       </section>

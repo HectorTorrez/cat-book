aed15c13ace3ed1b486ee0773dcd8801a9f3f874
Hector <torrezhectorb@gmail.com>
2023-09-02 20:30:26 -0600

    get data form the backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,52 @@
-import { Navbar } from "./features/Navbar";
-import { test } from "./API/test.json";
-import { CatCard } from "./features/CatCard";
-import { useState } from "react";
-import { UploadForm } from "./components/UploadForm";
+import { Navbar } from './features/Navbar'
+// import { test } from './API/test.json'
+import { CatCard } from './features/CatCard'
+import { useState, useEffect } from 'react'
+import { UploadForm } from './components/UploadForm'
+import { getCats } from './API/cats'
+import { type Cat } from './types/CatTypes'
 
-export const App = () => {
-  const [formIsActive, setFormIsActive] = useState(false);
-
-  const handleEsc = (event: KeyboardEvent) => {
-    if (event.key === "Escape") {
-      setFormIsActive(false);
+export const App = (): JSX.Element => {
+  const [formIsActive, setFormIsActive] = useState(false)
+  const [cats, setCats] = useState<Cat[]>([])
+  const getData = async (): Promise<void> => {
+    const cat = await getCats()
+    if (cats !== undefined) {
+      setCats(cat)
     }
-  };
-  window.addEventListener("keydown", handleEsc);
+  }
 
-  const handleActiveForm = (boolean: boolean) => {
-    setFormIsActive(boolean);
-  };
+  useEffect(() => {
+    void getData()
+  }, [])
+
+  const handleEsc = (event: KeyboardEvent): void => {
+    if (event.key === 'Escape') {
+      setFormIsActive(false)
+    }
+  }
+  window.addEventListener('keydown', handleEsc)
+
+  const handleActiveForm = (boolean: boolean): void => {
+    setFormIsActive(boolean)
+  }
 
   return (
     <section className="h-screen">
       <Navbar onHandle={handleActiveForm} formIsActive={formIsActive} />
       <section className="grid relative justify-items-center max-w-5xl m-auto sm:grid-cols-1 md:grid-cols-3 mt-10 ">
-        {test.map((cat) => {
-          return <CatCard key={cat.name} {...cat} />;
+        {cats.map((cat) => {
+          return <CatCard key={cat._id} {...cat} />
         })}
-        {formIsActive ? (
+        {formIsActive
+          ? (
           <UploadForm
             handleActiveForm={handleActiveForm}
             handleEsc={handleEsc}
           />
-        ) : null}
+            )
+          : null}
       </section>
     </section>
-  );
-};
+  )
+}

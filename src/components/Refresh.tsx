18716961848a884e296b9cd0c5462545dadba594
Hector <torrezhectorb@gmail.com>
2023-09-05 15:36:40 -0600

    new icon and new component

---
+++ src/components/Refresh.tsx
@@ -0,0 +1,14 @@
+import { Button } from './Button'
+import { Reload } from './Icons'
+
+interface RefreshProps {
+  handleRefresh?: () => void
+}
+export const Refresh = ({ handleRefresh }: RefreshProps): JSX.Element => {
+  return (
+   <section className='flex max-w-5xl m-auto justify-end mt-3'>
+     <Button text={<Reload/>} handleClick={handleRefresh}/>
+   </section>
+
+  )
+}

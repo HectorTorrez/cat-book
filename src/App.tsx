2dac0482b21f1abde3cb1701def72551ff82cd04
Hector <torrezhectorb@gmail.com>
2023-09-05 15:35:40 -0600

    new state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import { Navbar } from './features/Navbar'
-// import { test } from './API/test.json'
 import { CatCard } from './features/CatCard'
 import { useState, useEffect } from 'react'
 import { UploadForm } from './components/UploadForm'
 import { getCats } from './API/cats'
 import { type Cat } from './types/CatTypes'
+import { Refresh } from './components/Refresh'
 
 export const App = (): JSX.Element => {
   const [formIsActive, setFormIsActive] = useState(false)
   const [cats, setCats] = useState<Cat[]>([])
+  const [isLoading, setIsLoading] = useState('loading')
+
   const getData = async (): Promise<void> => {
     const cat = await getCats()
     if (cats !== undefined) {
       setCats(cat)
     }
+    setIsLoading('successful')
   }
 
   useEffect(() => {
     void getData()
-  }, [cats])
+  }, [formIsActive, isLoading])
 
   const handleEsc = (event: KeyboardEvent): void => {
     if (event.key === 'Escape') {
@@ -31,18 +34,36 @@ export const App = (): JSX.Element => {
     setFormIsActive(boolean)
   }
 
+  const handleRefresh = (): void => {
+    setIsLoading('loading')
+    setTimeout(() => {
+      setIsLoading('successful')
+    }, 500)
+  }
+
   return (
     <section className="h-screen">
       <Navbar onHandle={handleActiveForm} formIsActive={formIsActive} />
+      <Refresh handleRefresh={handleRefresh}/>
       <section className="grid relative justify-items-center max-w-5xl m-auto sm:grid-cols-1 md:grid-cols-3 mt-10 ">
-        {cats.map((cat) => {
-          return <CatCard key={cat._id} {...cat} />
-        })}
+        {
+          isLoading === 'loading'
+            ? null
+            : (
+              <>
+              {cats.map((cat) => {
+                return <CatCard key={cat._id} {...cat} />
+              })}
+        </>
+              )
+        }
+
         {formIsActive
           ? (
           <UploadForm
             handleActiveForm={handleActiveForm}
             handleEsc={handleEsc}
+            isUploadform
           />
             )
           : null}
